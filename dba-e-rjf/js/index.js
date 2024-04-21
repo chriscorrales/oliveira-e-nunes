70d727515a0df08857120452a2604d4e560e4d77
Christian Corrales <chriscorrales29@gmail.com>
2024-04-21 03:12:02 -0300

    Adiciona footer

--- dba-e-rjf/js/index.js
+++ dba-e-rjf/js/index.js
@@ -1,10 +1,28 @@
-jQuery(document).ready(function(){
-	jQuery('.aside-faq-icon').click(function(){
-		var toggle = jQuery(this).next('div#descwrapper');
+
+function showFaq() {
+  jQuery('.aside-faq-question-container').click(function(){
+    var toggle = jQuery(this).children('div#descwrapper');
 
 		jQuery(toggle).slideToggle("slow");
 	});
-    jQuery('.inactive').click(function(){
+
+  jQuery('.inactive').click(function(){
 		jQuery(this).toggleClass('inactive active');
 	});
-});
+}
+
+
+function debounce(func, timeout = 300){
+  let timer;
+  return (...args) => {
+    if (!timer) {
+      func.apply(this, args);
+    }
+    clearTimeout(timer);
+    timer = setTimeout(() => {
+      timer = undefined;
+    }, timeout);
+  };
+}
+
+jQuery(document).ready(debounce(showFaq, 1000));
